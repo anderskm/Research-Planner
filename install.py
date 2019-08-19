d7672b49fa380fd519b82a6478533a6062a7769b
Anders Krogh Mortensen <anderskroghm@gmail.com>
2019-08-19 13:56:13 +0200

    Catch errors and wait for user in install script

--- install.py
+++ install.py
@@ -3,7 +3,14 @@ import subprocess
 import sys
 
 def install():
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r' ,'requirements.txt'])
+    try:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
+        print('\nInstallation completed')
+    except Exception as ex:
+        print('\nAn error occured while installing the requied packages:')
+        print(ex)
+    finally:
+        input("\nPress Enter to continue...")
 
 # Example
 if __name__ == '__main__':
